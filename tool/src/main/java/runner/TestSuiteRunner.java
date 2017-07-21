f01747ed47a23731d224033a84fc9ce1aa3002f3
Andrea Stocco <tsigalko18@gmail.com>
2017-07-20 17:17:49 -0700

    added capability of running a test suite programmatically

--- tool/src/main/java/runner/TestSuiteRunner.java
+++ tool/src/main/java/runner/TestSuiteRunner.java
@@ -6,71 +6,42 @@ import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
 
-import main.java.claroline190.Claroline_TestSuite_Selenium;
-import main.java.datatype.*;
-import main.java.utils.*;
 import main.java.config.Settings;
+import main.java.datatype.EnhancedException;
+import main.java.utils.UtilsParser;
+import main.java.utils.UtilsRepair;
 
 public class TestSuiteRunner {
 
 	public static void main(String[] args) throws IOException {
-
-		// this step runs the test suite and the aspect records the visual
-		// execution trace
-		Result result1 = JUnitCore.runClasses(Claroline_TestSuite_Selenium.class);
-
-		for (Failure fail : result1.getFailures()) {
-			// for each breakage, I save the exception on the filesystem
-			//
-			UtilsRepair.saveFailures(fail);
-			EnhancedException ea = UtilsRepair.saveExceptionFromFailure(fail);
-
-			String path = Settings.testingTestSuiteVisualTraceExecutionFolder
-					+ UtilsRepair.capitalizeFirstLetter(ea.getFailedTest()) + Settings.javaExtension;
-			String jsonPath = UtilsParser.toJsonPath(path);
-
-			UtilsParser.serializeException(ea, jsonPath);
+		
+		// path to the test suite class name
+//		String classRunner = "main.java.claroline.TestLoginAdmin";
+		String classRunner = "main.java.clarolineDirectBreakage.TestLoginAdmin";
+		
+		// this step runs the test suite and the aspect records the visual execution trace 
+		Result result = null;
+		try {
+			result = JUnitCore.runClasses(Class.forName(classRunner));
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
 		}
-
-		// step 3 : run the analysis and repair
-
+		
+		// if tests have failed, save the exception
+		if(!result.wasSuccessful()) {
+		
+			// for each breakage, I save the exception on the filesystem
+			for (Failure fail : result.getFailures()) {
+				
+				UtilsRepair.saveFailures(fail);
+				EnhancedException ea = UtilsRepair.saveExceptionFromFailure(fail);
+				
+				String path = Settings.testingTestSuiteVisualTraceExecutionFolder + UtilsRepair.capitalizeFirstLetter(ea.getFailedTest()) + Settings.javaExtension;
+				String jsonPath = UtilsParser.toJsonPath(path);
+				
+				UtilsParser.serializeException(ea, jsonPath);
+			}
+		}
 	}
-
-	// public void runSingleTest(String test) {
-	//
-	// if (Settings.INRECORDING) {
-	//
-	// // I want to record the visual trace of the correct test case
-	//
-	// // Result result =
-	// // JUnitCore.runClasses(Claroline_TestSuite_Selenium.class);
-	// // for (Failure fail : result.getFailures()) {
-	// // System.out.println(fail.toString());
-	// // }
-	// // assertTrue(result.wasSuccessful());
-	//
-	// } else {
-	// // I want to run the test case of the next test suite
-	//
-	// }
-	// }
-	//
-	// public void runAllTests() {
-	//
-	// if (Settings.INRECORDING) {
-	//
-	// // I want to record the visual trace of the correct test suite
-	//
-	// // Result result =
-	// // JUnitCore.runClasses(Claroline_TestSuite_Selenium.class);
-	// // for (Failure fail : result.getFailures()) {
-	// // System.out.println(fail.toString());
-	// // }
-	// // assertTrue(result.wasSuccessful());
-	//
-	// } else {
-	// // I want to run the next test suite
-	// }
-	// }
 
 }
