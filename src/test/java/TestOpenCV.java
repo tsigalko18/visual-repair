6f855723a9c8c51c6e74b51d3d273b0533977728
Andrea Stocco <tsigalko18@gmail.com>
2017-11-20 17:49:58 -0800

    test of new template matching

--- src/test/java/TestOpenCV.java
+++ src/test/java/TestOpenCV.java
@@ -1,9 +1,10 @@
 
-
 import org.junit.Before;
 import org.junit.Test;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+
+import utils.UtilsComputerVision;
 
 public class TestOpenCV {
 
@@ -12,10 +13,20 @@ public class TestOpenCV {
 		nu.pattern.OpenCV.loadShared();
 	}
 
+//	@Test
+//	public void test() {
+//		Mat mat = Mat.eye(3, 3, CvType.CV_8UC1);
+//		System.out.println("mat = " + mat.dump());
+//	}
+
 	@Test
-	public void test() {
-		Mat mat = Mat.eye(3, 3, CvType.CV_8UC1);
-		System.out.println("mat = " + mat.dump());
+	public void testTM() {
+
+		String bookObject = "src/test/resources/indexsmall.jpg";
+		String bookScene = "src/test/resources/index.jpg";
+
+		UtilsComputerVision.returnAllMatches(bookScene, bookObject);
+		
 	}
 
 }
