5d02445c530e2cdfa9338265c413e7462edf2b14
Andrea Stocco <tsigalko18@gmail.com>
2018-01-20 20:19:35 -0800

    fixed clear case

--- src/main/java/datatype/EnhancedWebElement.java
+++ src/main/java/datatype/EnhancedWebElement.java
@@ -5,24 +5,30 @@ import org.openqa.selenium.support.ui.Select;
 public class EnhancedWebElement extends Statement {
 
 	// web element != select
-	public Select getSelect() { return null; }
-	public void setSelect(Select we) { select = null; }
-	
-	public EnhancedWebElement() {
-		super();
-	}
-	
-	@Override
-	public String toString() {
-		
-		if(getAction().equals("sendKeys")) {
-			return "driver.findElement(" + getDomLocator() + ")." + getAction() + "(\"" + getValue() + "\")";
-		} else if(getAction().equals("click")) {
-			return "driver.findElement(" + getDomLocator() + ")." + getAction() + "(" + getValue() + ")";
-		}
-		
+	public Select getSelect() {
 		return null;
 	}
 
-	
+	public void setSelect(Select we) {
+		select = null;
+	}
+
+	public EnhancedWebElement() {
+		super();
+	}
+
+	@Override
+	public String toString() {
+
+		if (getAction().equals("sendKeys")) {
+			return "driver.findElement(" + getDomLocator() + ")." + getAction() + "(\"" + getValue() + "\")";
+		} else if (getAction().equals("click")) {
+			return "driver.findElement(" + getDomLocator() + ")." + getAction() + "(" + getValue() + ")";
+		} else if (getAction().equals("clear")) {
+			return "driver.findElement(" + getDomLocator() + ")." + getAction() + "(" + getValue() + ")";
+		}
+
+		return null;
+	}
+
 }
